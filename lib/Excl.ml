bb321b75c8d3cf2fc9ed6e3beac6dcc6e2139742
Oscar Sjöstedt <n1ark.officiel@gmail.com>
2024-04-18 13:30:58 +0100

    Fix Excl ! Should be good now

--- lib/Excl.ml
+++ lib/Excl.ml
@@ -2,7 +2,9 @@ open Gillian.Utils
 open Gillian.Monadic
 open Gillian.Symbolic
 open Gil_syntax
+
 module DR = Delayed_result
+module Recovery_tactic = Gillian.General.Recovery_tactic
 
 type init_data = unit
 
@@ -14,8 +16,6 @@ type c_fix_t =
   | FAddState of vt
 type err_t =
   | MissingState
-  | DoubleAlloc
-  | DoubleFree
 [@@deriving show, yojson]
 
 type t =
@@ -25,6 +25,7 @@ type t =
 
 type action_ret = (t * vt list, err_t) result
 
+let core_pred ?(ins=[]) ?(outs=[]) p = Asrt.GA (p, ins, outs)
 
 let pp_v v = Yojson.Safe.to_string (Values.to_yojson v)
 let pp_t v = match v with
@@ -35,50 +36,32 @@ let pp_t v = match v with
 let init (i:init_data) : t = None
 let get_init_data (i:t) : init_data = ()
 
-let clear (v:t) : t =
-  failwith "Implement here (clear)"
-
-let execute_alloc v args = match v, args with
-  | None, [v] -> Ok (Val v, [])
-  | Val _, _ -> Error DoubleAlloc
-  | _, _ -> failwith "Invalid alloc call"
-
-let execute_free v args = match v, args with
-  | None, _ -> Error DoubleFree
-  | Val _, [] -> Ok (None, [])
-  | _, _ -> failwith "Invalid free call"
-
-let execute_load v args = match v, args with
-  | None, _ -> Error MissingState
-  | Val v, [] -> Ok (Val v, [v])
-  | _, _ -> failwith "Invalid load call"
-
-let execute_store v args = match v, args with
-  | None, _ -> Error MissingState
-  | Val v, [v'] -> Ok (Val v', [])
-  | _, _ -> failwith "Invalid store call"
+let clear (v:t) : t = None
 
 (* val execute_action : action_name:string -> t -> vt list -> action_ret Delayed.t*)
 let execute_action ~(action_name:string) (v:t) (args:vt list) : action_ret Delayed.t =
-  let res = match action_name with
-    | "load" -> execute_load v args
-    | "store" -> execute_store v args
-    | "alloc" -> execute_alloc v args
-    | "free" -> execute_free v args
+  match action_name, v, args with
+    | "load", None, _ -> DR.error MissingState
+    | "load", Val v, [] -> DR.ok (Val v, [v])
+    | "store", None, _ -> DR.error MissingState
+    | "store", Val v, [v'] -> DR.ok (Val v', [])
     | _ -> failwith (Printf.sprintf "Unrecognized action: %s" action_name)
-  in match res with
-    | Ok (v', args') -> Delayed.return (Ok (v', args'))
-    | Error e -> Delayed.return (Error e)
 
   (* val consume : core_pred:string -> t -> vt list -> action_ret Delayed.t *)
 let consume ~(core_pred:string) (a:t) (args:vt list) : action_ret Delayed.t =
-  failwith (Printf.sprintf "Implement here (consume %s)" core_pred)
+  match core_pred, a, args with
+  | "points_to", Val v, [] -> DR.ok (None, [v])
+  | "points_to", None, _ -> DR.error MissingState
+  | _ -> failwith (Printf.sprintf "Invalid core predicate call: %s" core_pred)
 
   (* val produce : core_pred:string -> t -> vt list -> t Delayed.t*)
 let produce ~(core_pred:string) (a:t) (args:vt list) : t Delayed.t =
-  failwith (Printf.sprintf "Implement here (produce %s)" core_pred)
+  match core_pred, a, args with
+  | "points_to", None, [v] -> Delayed.return (Val v)
+  | "points_to", Val _, _ -> Delayed.vanish ()
+  | _ -> failwith (Printf.sprintf "Unrecognized core predicate: %s" core_pred)
 
-let is_overlapping_asrt _ = failwith "Implement here (is_overlapping_asrt)"
+let is_overlapping_asrt a = false
 
 let copy v = v
 
@@ -102,36 +85,25 @@ let lvars s = match s with
   | Val v -> Expr.lvars v
 let alocs s = Containers.SS.empty
 
-(* val assertions : ?to_keep:Containers.SS.t -> t -> Asrt.t list *)
-let assertions ?(to_keep=Containers.SS.empty) _ = failwith "Implement here (assertions)"
+let assertions ?(to_keep=Containers.SS.empty) s: Asrt.t list = match s with
+  | None -> []
+  | Val v -> [core_pred "points_to" ~outs:[v]]
 let mem_constraints s = []
 let pp_c_fix fmt _ = Format.fprintf fmt "fix"
-let get_recovery_tactic _ _ = failwith "Implement here (get_recovery_tactic)"
+let get_recovery_tactic (s:t) (e:err_t): vt Recovery_tactic.t = match e with
+  (* | MissingState -> Recovery_tactic.try_unfold ??? *)
+  | _ -> Recovery_tactic.none
+
 let pp_err fmt e = Format.fprintf fmt "%s" (show_err_t e)
 let get_failing_constraint e = failwith "Implement here (get_failing_constraint)"
 
-(* (c_fix_t list * Formula.t list * (string * Type.t) list * Containers.SS.t)
-list
-*)
-
-let fresh_name =
-  let counter = ref 0 in
-  fun () ->
-    let n = !counter in
-    counter := !counter + 1;
-    Printf.sprintf "fresh_%d" n
-
 let get_fixes s pfs tenv = function
-| MissingState -> [
-  ([FAddState (LVar (fresh_name ()))], [], [], Containers.SS.empty)
-]
-| _ -> []
+  | MissingState -> [
+    ([FAddState (LVar (Generators.fresh_svar ()))], [], [], Containers.SS.empty)
+  ]
 
-let can_fix = function
-| MissingState -> true
-| _ -> false
-let apply_fix s = function
-| FAddState v -> DR.ok (Val v)
+let can_fix = function | MissingState -> true
+let apply_fix s = function | FAddState v -> DR.ok (Val v)
 let pp_by_need _ = pp
 let get_print_info _ _ = (Containers.SS.empty, Containers.SS.empty)
 let sure_is_nonempty _ = false
